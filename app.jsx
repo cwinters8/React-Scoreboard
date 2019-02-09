5925cba3f7abfa41869ec635af52a45159300c5b
cwinters8 <clwinters8@gmail.com>
2019-02-08 18:29:00 -0800

    add decrement score and improve state functions

--- app.jsx
+++ app.jsx
@@ -43,16 +43,26 @@ class Counter extends React.Component {
         score: 0
     }
 
+    decrementScore = () => {
+        this.setState(prevState => {
+            return {
+                score: prevState.score - 1
+            };
+        });
+    }
+
     incrementScore = () => {
-        this.setState({
-            score: this.state.score + 1
+        this.setState(prevState => {
+            return {
+                score: prevState.score + 1
+            };
         });
     }
 
     render() {
         return (
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+                <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
                 <span className="counter-score">{this.state.score}</span>
                 <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
